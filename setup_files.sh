84c122dfdbc57a5a43ba5faa60086c441eb810aa
Karim-308 <2001118@eng.asu.edu.eg>
2024-08-30 03:32:13 +0300

    Lets make it responsive

---
+++ setup_files.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Copy the content of 10-index.html into 12-index.html
+cp 0x00-html_advanced/10-index.html 0x00-html_advanced/12-index.html
+
+# Copy the contents of 11-styleguide.html into 13-styleguide.html
+cp 0x00-html_advanced/11-styleguide.html 0x00-html_advanced/13-styleguide.html
+
+# Copy the contents of 12-index.html into 14-index.html
+cp 0x00-html_advanced/12-index.html 0x00-html_advanced/14-index.html
+
+# Copy the contents of 14-index.html into 15-index.html
+cp 0x00-html_advanced/14-index.html 0x00-html_advanced/15-index.html
+
+# Copy the contents of 15-index.html into 16-index.html
+cp 0x00-html_advanced/15-index.html 0x00-html_advanced/16-index.html
+
+# Copy the contents of 16-index.html into 17-index.html
+cp 0x00-html_advanced/16-index.html 0x00-html_advanced/17-index.html
+
+# Copy the contents of 17-index.html into 18-index.html
+cp 0x00-html_advanced/17-index.html 0x00-html_advanced/18-index.html
+
+# Create about.html, latest_news.html, and contact.html based on 18-index.html
+cp 0x00-html_advanced/18-index.html 0x00-html_advanced/about.html
+cp 0x00-html_advanced/18-index.html 0x00-html_advanced/latest_news.html
+cp 0x00-html_advanced/18-index.html 0x00-html_advanced/contact.html
+
+# Modify titles in the new files
+sed -i 's/Homepage/About/' 0x00-html_advanced/about.html
+sed -i 's/Homepage/Latest news/' 0x00-html_advanced/latest_news.html
+sed -i 's/Homepage/Contact/' 0x00-html_advanced/contact.html
+
+# Copy the contents of 18-index.html into 20-index.html
+cp 0x00-html_advanced/18-index.html 0x00-html_advanced/20-index.html
+
+# Copy the contents of 20-index.html into 21-index.html
+cp 0x00-html_advanced/20-index.html 0x00-html_advanced/21-index.html
+
+# Copy the contents of 21-index.html into 22-index.html
+cp 0x00-html_advanced/21-index.html 0x00-html_advanced/22-index.html
+
+# Copy the contents of 22-index.html into 23-index.html
+cp 0x00-html_advanced/22-index.html 0x00-html_advanced/23-index.html
+
+# Copy the contents of 23-index.html into 24-index.html
+cp 0x00-html_advanced/23-index.html 0x00-html_advanced/24-index.html
+
+# Copy the contents of 24-index.html into 25-index.html
+cp 0x00-html_advanced/24-index.html 0x00-html_advanced/25-index.html
+
+# Copy the contents of 13-styleguide.html into 26-styleguide.html
+cp 0x00-html_advanced/13-styleguide.html 0x00-html_advanced/26-styleguide.html
+
+# Copy the contents of 25-index.html into 27-index.html
+cp 0x00-html_advanced/25-index.html 0x00-html_advanced/27-index.html
+
+# Copy the contents of 26-styleguide.html into 28-styleguide.html
+cp 0x00-html_advanced/26-styleguide.html 0x00-html_advanced/28-styleguide.html
+
+# Copy the contents of 27-index.html into 29-index.html
+cp 0x00-html_advanced/27-index.html 0x00-html_advanced/29-index.html
+
+# Copy the contents of 28-styleguide.html into 30-styleguide.html
+cp 0x00-html_advanced/28-styleguide.html 0x00-html_advanced/30-styleguide.html
+
+# Copy the contents of 29-index.html into 31-index.html
+cp 0x00-html_advanced/29-index.html 0x00-html_advanced/31-index.html
+
+# Copy the contents of 30-styleguide.html into 32-styleguide.html
+cp 0x00-html_advanced/30-styleguide.html 0x00-html_advanced/32-styleguide.html
+
+# Copy the contents of 32-styleguide.html into 33-styleguide.html
+cp 0x00-html_advanced/32-styleguide.html 0x00-html_advanced/33-styleguide.html
+
+# Copy the contents of 33-styleguide.html into 34-styleguide.html
+cp 0x00-html_advanced/33-styleguide.html 0x00-html_advanced/34-styleguide.html
+
+# Copy the contents of 31-index.html into 35-index.html
+cp 0x00-html_advanced/31-index.html 0x00-html_advanced/35-index.html
+
+# Copy the contents of 35-index.html into 36-index.html
+cp 0x00-html_advanced/35-index.html 0x00-html_advanced/36-index.html
+
+# Copy the contents of 36-index.html into index.html (the final file)
+cp 0x00-html_advanced/36-index.html 0x00-html_advanced/index.html
+
+# Copy the contents of 34-styleguide.html into 38-styleguide.html
+cp 0x00-html_advanced/34-styleguide.html 0x00-html_advanced/38-styleguide.html
+
+# Copy the contents of 38-styleguide.html into 39-styleguide.html
+cp 0x00-html_advanced/38-styleguide.html 0x00-html_advanced/39-styleguide.html
+
+# Copy the contents of 39-styleguide.html into styleguide.html (final file)
+cp 0x00-html_advanced/39-styleguide.html 0x00-html_advanced/styleguide.html
+
